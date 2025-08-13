794c92ff2d2bda47cab39885fb2de026b4745437
Kanan Ismayilzada <7609260+Kenan125@users.noreply.github.com>
2025-08-13 13:32:52 +0300

    phone validation

--- frontend/src/taskpane/ReadColumn.ts
+++ frontend/src/taskpane/ReadColumn.ts
@@ -1,3 +1,4 @@
+import { parsePhoneNumberWithError } from "libphonenumber-js";
 /* global Excel  */
 export async function readColumn(chsRow:number): Promise<any[]> {
     try {
@@ -12,6 +13,41 @@ export async function readColumn(chsRow:number): Promise<any[]> {
             console.log(text);
 
             }
+            const phoneNumber = parsePhoneNumberWithError(" 0 (800) 555-35-35 ", "TR");
+
+
+
+      if (phoneNumber) {
+
+
+        console.log(phoneNumber)
+
+
+        phoneNumber.number === "+75313987280";
+
+
+        console.log("country"+ phoneNumber.country)
+
+
+        console.log(phoneNumber.isPossible());
+
+
+        //phoneNumber.country === "TR";
+
+
+        //phoneNumber.isPossible() === true;
+
+
+        phoneNumber.isValid() === true;
+
+
+        // Note: `.getType()` requires `/max` metadata: see below for an explanation.
+
+
+        phoneNumber.getType() === "TOLL_FREE";
+
+
+      }
             
 
             return text;
